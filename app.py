7d02f8ac4a5cd665e70a04516ed662d6def94357
AnnaRaman <anna_raman@hotmail.co.uk>
2020-11-16 08:37:12 +0000

    add get recipe route decorator

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
-    request, session, url_for)
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
 
-app.config["MONGO_DBBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
-@app.route("/get_names")
-def get_names():
-    names = mongo.db.names.find()
-    return render_template("recipes.html", recipes=recipes)
+@app.route("/get_recipe")
+def get_recipe():
+    recipe = mongo.db.recipe.find()
+    return render_template("recipes.html", recipe=recipe)
 
 
 if __name__ == "__main__":
